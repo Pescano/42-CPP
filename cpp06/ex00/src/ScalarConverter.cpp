b0ffe8606363b84669765e005b001828f1aeaa06
paescano <88772018+Pescano@users.noreply.github.com>
2023-12-20 17:48:44 +0100

    cpp06 fix

--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: paescano <paescano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 17:24:02 by paescano          #+#    #+#             */
-/*   Updated: 2023/12/18 13:07:37 by paescano         ###   ########.fr       */
+/*   Updated: 2023/12/19 12:49:03 by paescano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,9 @@ void ScalarConverter::convert(const std::string& input)
 	int _int = 0;
 	float _float = 0;
 	double _double = 0;
+	bool isFloat = true;
+	bool isDouble = true;
+	bool isInt = true;
 
 	if (input == "inf" || input == "inff" || \
         input == "+inf" || input == "+inff")
@@ -69,31 +72,66 @@ void ScalarConverter::convert(const std::string& input)
 		return;
 	}
 
-	_int = std::atoi(input.c_str());
-	
-	if (_char == "" && std::isprint(_int)) 
+	try
 	{
-		_char = "'";
-		_char += static_cast<char>(_int);
-		_char += "'";
-	} 
-	else if (_char == "") 
-	{
-		_char = "Non displayable";
+		_int = std::stoi(input);
 	}
+	catch(const std::exception& e)
+	{
+		isInt = false;
+	}
+	
+	if (isInt)
+	{
+		if (std::isprint(_int))
+		{
+			_char = "'";
+			_char += static_cast<char>(_int);
+			_char += "'";
+		}
+		else
+			_char = "Non displayable";
+	}
+	else
+		_char = "imposible";
+
+	std::cout << "char:   " << _char << std::endl;
+	if (isInt)
+		std::cout << "int:    " << _int << std::endl;
+	else
+		std::cout << "int:    imposible" << std::endl;
 
 	if (input[input.length() - 1] == 'f') 
 	{
-		_float = std::atof(input.c_str());
-		_double = static_cast<double>(_float);
+		try
+		{
+			_float = std::stof(input.c_str());
+			_double = static_cast<double>(_float);
+		}
+		catch(const std::exception& e)
+		{
+			isFloat = false;
+		}
 	} else 
 	{
-		_double = std::atof(input.c_str());
-		_float = static_cast<float>(_double);
+		try
+		{
+			_double = std::stod(input.c_str(), NULL);
+			_float = static_cast<float>(_double);
+		}
+		catch(const std::exception& e)
+		{
+			isDouble = false;
+		}
 	}
 
-	std::cout << "char:   " << _char << std::endl;	
-	std::cout << "int:    " << _int << std::endl;
+	if (!isFloat || !isDouble) 
+	{
+		std::cout << "float:  imposible" << std::endl;
+		std::cout << "double: imposible" << std::endl;
+		return;
+	}
+
 	if (_float - static_cast<int>(_float) == 0) 
 	{
 		std::cout << "float:  " << _float << ".0f" << std::endl;
